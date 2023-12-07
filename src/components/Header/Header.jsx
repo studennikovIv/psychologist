c21fc657735f42b6fb2f6dec74c1aa73fb3566e0
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-12-07 22:57:52 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,12 +8,12 @@ export const Header = () =>{
             <header className='header'>
         <img src={headerImg} alt="Логотип" />
         <nav>
-            <a href="#">Меню</a>
-            <a href="#">О мне</a>
-            <a href="#">Отзывы</a>
-            <a href="#">Услуги</a>
-            <a href="#">Информация</a>
-            <a href="#">Статьи</a>
+            <a href="">Меню</a>
+            <a href="">О мне</a>
+            <a href="">Отзывы</a>
+            <a href="">Услуги</a>
+            <a href="">Информация</a>
+            <a href="">Статьи</a>
         </nav>
         <div className="phone-numbers">
             <a href="tel:+89054959528"> 8(905)495-95-28</a>
