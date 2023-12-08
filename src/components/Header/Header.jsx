e6c0aa50c915b78d2609712b72b16cf68637d040
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-12-08 19:36:30 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import headerImg from '../../images/header logo.png'
+import headerRegistrationIcon from '../../images/registrationIcon.png'
 import  './style.css'
 export const Header = () =>{
     return(
@@ -6,21 +7,25 @@ export const Header = () =>{
     
         <div className='conteiner'>
             <header className='header'>
-        <img src={headerImg} alt="Логотип" />
-        <nav className='navList'>
-            <a href="google.com">Меню</a>
-            <a href="google.com">О мне</a>
-            <a href="google.com">Отзывы</a>
-            <a href="google.com">Услуги</a>
-            <a href="google.com">Информация</a>
-            <a href="google.com">Статьи</a>
-        </nav>
-        <div className="telHeader">
-            <a href="tel:+89054959528"> 8(905)495-95-28</a>
-            <a href="tel:+89180427687"> 8(918)042-76-87</a>
-        </div>
-        <button id="register-button">Регистрация</button>
-        </header>
+               <a href="google.com"> <img src={headerImg} alt="Логотип" /></a>
+                <nav className='navList'>
+                    <a className='active' href="/">Меню</a>
+                    <a href="/">Обо мне</a>
+                    <a href="/">Отзывы</a>
+                    <a href="/">Услуги</a>
+                    <a href="/">Информация</a>
+                    <a href="/">Статьи</a>
+                </nav>
+                <div className="telHeader">
+                    <a href="tel:+89054959528"> 8(905)495-95-28</a>
+                    <a href="tel:+89180427687"> 8(918)042-76-87</a>
+                </div>
+
+                <button className="registerButton">
+                    <img src={headerRegistrationIcon} alt="icon" />
+                    Регистрация
+                </button>
+            </header>
         </div>
     
     )
