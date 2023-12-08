2d17515f6194aa62f40de923690a45497b34392c
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-12-08 10:20:29 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () =>{
         <div className='conteiner'>
             <header className='header'>
         <img src={headerImg} alt="Логотип" />
-        <nav>
+        <nav className='navList'>
             <a href="google.com">Меню</a>
             <a href="google.com">О мне</a>
             <a href="google.com">Отзывы</a>
@@ -15,7 +15,7 @@ export const Header = () =>{
             <a href="google.com">Информация</a>
             <a href="google.com">Статьи</a>
         </nav>
-        <div className="phone-numbers">
+        <div className="telHeader">
             <a href="tel:+89054959528"> 8(905)495-95-28</a>
             <a href="tel:+89180427687"> 8(918)042-76-87</a>
         </div>
