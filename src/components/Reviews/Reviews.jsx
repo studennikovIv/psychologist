b8c01416cea833c4aea1e630bc8eeb8dab37ed0c
grant956 <68757554+studennikovIv@users.noreply.github.com>
2024-02-25 21:22:07 +0200

    slider

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,12 +1,36 @@
 import  './style.css'
+import { useState } from 'react';
+import img from '../../images/aboutMe.jpg'
+import img2 from '../../images/imgBgHero.png'
 
 
 export const Reviews = () =>{
+  const images = [
+    img,
+    img2,
+    img,
+    
+  ];
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+  
+    const prevSlide = () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    };
     return(
     <div className="reviews">
         <div className="conteiner">
             <h3>Отзывы</h3>
-
+            <div className="slider">
+                <button onClick={prevSlide}>Previous</button>
+                <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+                <button onClick={nextSlide}>Next</button>
+                </div>
         </div>
     </div>
 
